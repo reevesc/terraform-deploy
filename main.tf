f497c60b705e397da182d08e6a9368e3076aea11
Clinton Reeves <contact@clintonreeves.com>
2023-11-20 13:37:55 -0700

    add missing terraform configuration

--- main.tf
+++ main.tf
@@ -23,7 +23,25 @@ data "aws_ami" "latest_amazon_linux" {
 }
 
 data "aws_vpc" "selected_vpc" {
-    filter {
-        name = "tag:Name"
-    }
+	filter {
+	   name = "tag:Name"
+	   values = [var.vpc_name]
+	}
+	
+}
+
+data "aws_subnets" "private" {
+	filter {
+	   name = "vpc-id"
+	   values = [data.aws_vpc.selected_vpc.id]
+	}
+	tags = {
+		Type = "private"
+	}
+}
+
+resource "aws_instance" "ec2_instance" {
+	ami = data.aws_ami.latest_amazon_linux.id
+        instance_type = "t2.micro"
+	subnet_id = data.aws_subnets.private.ids[0]
 }
